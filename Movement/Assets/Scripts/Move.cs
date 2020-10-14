b6cdd3e04829a2b80bd2f64ff25d921ec8949b4b
kunenemdu <61043066+kunenemdu@users.noreply.github.com>
2020-10-14 18:09:45 +0400

    changes

---
+++ Movement/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 10f;
+	Rigidbody r;
+	private void Start()
+    {
+		r = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		r.transform.Translate(x, 0, z);
+
+        //jump cube
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+	}
+}
